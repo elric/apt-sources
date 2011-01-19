a8a7c2ee20574727c24219e81395b5e592d4f675
Omar Campagne <ocampagne@gmail.com>
2011-01-20 00:21:54 +0100

    0.5

--- aptsources.sh
+++ aptsources.sh
@@ -11,25 +11,11 @@
 #
 #        AUTHOR: Omar Campagne
 #       CREATED: 07/01/11 17:16:35 CET
-#       VERSION: 0.4
+#       VERSION: 0.5
 #
 #       Credits to Anant Shirvastava [1], add_lp_repo() is his.
 #       [1] http://blog.anantshri.info/howto-add-ppa-in-debian/    
 #===============================================================================
-
-# TODO: Only one argument a time is allowed
-# check if already enabled or disabled
-# give option to apt-get update after command
-# autoinstall's sed can be nicer
-# check proper bash script logging | output
-# -lp: apt-get update is used to get the key, and another upd is 
-# necessary. so use wget from the other script 
-# bash autocompletion
-
-# check_lines doesn't work with -d because grep doesn't tell if one or both
-# lines are commented. Therefore, -d fails because a repo counts as
-# $sources_enabled when only the deb line is uncommented.  I need 3 states,
-# enabledbin, enabledsrc and disabled, or sthg like that.
 
 #-------------------------------------------------------------------------------
 # Functions
