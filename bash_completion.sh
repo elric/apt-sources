b6db288b1c55773fd5ba81defc82b2a19bb4b302
Omar Campagne <ocampagne@gmail.com>
2011-01-19 13:56:20 +0100

    bash completion finished

--- bash_completion.sh
+++ bash_completion.sh
@@ -1,36 +1,52 @@
-## Completion file for aptsources
-_aptsources () 
+##  Bash cmpletion file for aptsources
+sources () 
 {
-    local command previous_command opts
+    sources=$( ls -o -g /etc/apt/sources.list.d/| grep '.list$' | awk '{print $7}' | sed 's/.list/\ /g' )
+
+    for file in $sources ; do
+        grep '^ *deb' /etc/apt/sources.list.d/"$file".list >> /dev/null
+
+        if [ $? == 0 ] ; then
+            sources_enabled="$sources_enabled $file";    # holds files with uncommented 
+                                                         # lines
+        else
+            sources_disabled="$sources_disabled $file";  # the opposite
+        fi
+    done
+}
+
+_aptsources ()
+{
+    local command opts
     COMPREPLY=()
 
-    # List sources files
-    sources=$( ls -o -g /etc/apt/sources.list.d/| grep '.list$' | awk '{print $7}' | sed 's/.list/\ /g' )
-    
     command="${COMP_WORDS[COMP_CWORD]}" # User input
-    previous_command="${COMP_WORDS[COMP_CWORD-1]}" # shift arguments and get value
     
     # Options to show
-    opts=" --enable --disable --src --list --add --remove --add-launchpad --help --list "
+    opts=" --enable --disable --src --show-source --add --remove --add-launchpad --help --list "
     
     ## If the introduced command is one of the following, keep on completing
     ## sources
     case "${COMP_WORDS[1]}" in
-	--enable|--disable|--src|--remove)
-	COMPREPLY=( $(compgen -W "${sources}" -- ${command}))
-    esac
-
-    ## Don't complete these options
-    case "${previous_command}" in
+	--enable)
+            COMPREPLY=( $(compgen -W "${sources_disabled}" -- ${command}))
+            return 0;;
+	--disable)
+	    COMPREPLY=( $(compgen -W "${sources_enabled}" -- ${command}))
+            return 0;;
+	--src|--show-source|--remove)
+	    COMPREPLY=( $(compgen -W "${sources}" -- ${command}))
+            return 0;;
         --help|--list|--add|--add-launchpad)
-        return 0
+            return 0;;
     esac
 
     # If no option is specified show all options
     if [[ ${command} = -* ]]; then
-    COMPREPLY=( $(compgen -W "${opts}" -- ${command}) )
-    return 0
+        COMPREPLY=( $(compgen -W "${opts}" -- ${command}) )
+        return 0
     fi
 }
 
+sources
 complete -F _aptsources aptsources
